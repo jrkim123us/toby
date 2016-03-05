dec5ac799931684888002dbe6364f286c8f9b793
Kim Jong Rok <jrkim123us@gmail.com>
2016-03-05 16:43:49 +0900

    callback 응용 적용

--- src/test/java/learningtest/template/CalcSumTest.java
+++ src/test/java/learningtest/template/CalcSumTest.java
@@ -1,20 +1,30 @@
 package learningtest.template;
 
-import java.io.IOError;
-import java.io.IOException;
-
-import org.hamcrest.core.Is;
-import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class CalcSumTest {
+	Calculator calculator;
+	String numFilepath;
+	
+	@Before
+	public void setUp() {
+		this.calculator = new Calculator();
+		this.numFilepath = getClass().getResource("numbers.txt").getPath();
+	}
+	
 	@Test
 	public void sumOfNumbers() throws IOException {
-		Calculator calculator = new Calculator();
-		int sum = calculator.calcSum(getClass().getResource("numbers.txt").getPath());
-		assertThat(sum, is(10));
-		
+		assertThat(this.calculator.calcSum(this.numFilepath), is(10));
+	}
+	
+	@Test
+	public void multiplyOfNumbers() throws IOException {
+		assertThat(this.calculator.calcMultiply(this.numFilepath), is(24));
 	}
 }
--- src/test/java/learningtest/template/Calculator.java
+++ src/test/java/learningtest/template/Calculator.java
@@ -5,17 +5,12 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Calculator {
-	public Integer calcSum(String filepath) throws IOException {
+	public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
 		BufferedReader bufferedReader = null;
 		try {
 			bufferedReader = new BufferedReader(new FileReader(filepath));
-			Integer sum = 0;
-			String line = null;
-			while((line = bufferedReader.readLine()) != null) {
-				sum += Integer.valueOf(line);
-			}
-			bufferedReader.close();
-			return sum;
+			int result = callback.doSomethingWithReader(bufferedReader);
+			return result;
 		} catch(IOException e) {
 			System.out.println(e.getMessage());
 			throw e;
@@ -25,7 +20,38 @@ public class Calculator {
 				catch(IOException e) { System.out.println(e.getMessage());}
 			}
 		}
-		
-		
+	}
+	public Integer calcSum(String filepath) throws IOException {
+		BufferedReaderCallback sumCallback = 
+			new BufferedReaderCallback() {				
+				@Override
+				public Integer doSomethingWithReader(BufferedReader bufferedReader) throws IOException {
+					Integer sum = 0;
+					String line = null;
+					while((line = bufferedReader.readLine()) != null) {
+						sum += Integer.valueOf(line);
+					}
+					
+					return sum;
+				}
+			};
+		return this.fileReadTemplate(filepath, sumCallback);
+	}
+	
+	public Integer calcMultiply(String filepath) throws IOException {
+		BufferedReaderCallback multiplyCallback = 
+				new BufferedReaderCallback() {				
+					@Override
+					public Integer doSomethingWithReader(BufferedReader bufferedReader) throws IOException {
+						Integer multiply = 1;
+						String line = null;
+						while((line = bufferedReader.readLine()) != null) {
+							multiply *= Integer.valueOf(line);
+						}
+						
+						return multiply;
+					}
+				};
+			return this.fileReadTemplate(filepath, multiplyCallback);
 	}
 }
