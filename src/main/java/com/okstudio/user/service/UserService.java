cc05d129c34d72d708d9c8c577fe54bd65c7b26e
Kim Jong Rok <jrkim123us@gmail.com>
2016-03-06 10:33:42 +0900

    코드 리팩토링 - 01

--- src/main/java/com/okstudio/user/service/UserService.java
+++ src/main/java/com/okstudio/user/service/UserService.java
@@ -16,21 +16,8 @@ public class UserService {
 	public void upgradeLevels() {
 		List<User> users = userDao.getAll();
 		for(User user : users) {
-			Boolean isChanged = null;
-			if(user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
-				user.setLevel(Level.SILVER);
-				isChanged = true;
-			} else if(user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
-				user.setLevel(Level.GOLD);
-				isChanged = true;
-			} else if(user.getLevel() == Level.GOLD) {
-				isChanged = false;
-			} else {
-				isChanged = false;
-			}
-			
-			if(isChanged) {
-				userDao.update(user);
+			if(this.canUpgradeLevel(user)) {
+				this.upgradeLevel(user);
 			}
 		}
 	}
@@ -40,5 +27,24 @@ public class UserService {
 			user.setLevel(Level.BASIC);
 		}
 		userDao.add(user);		
+	}
+	
+	private boolean canUpgradeLevel(User user) {
+		Level currentLevel = user.getLevel();
+		switch(currentLevel) {
+			case BASIC : return (user.getLogin() >= 50);
+			case SILVER : return (user.getRecommend() >= 30);
+			case GOLD : return false;
+			default : throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+		}
+	}
+	
+	private void upgradeLevel(User user) {
+		if(user.getLevel() == Level.BASIC) {
+			user.setLevel(Level.SILVER);			
+		} else if(user.getLevel() == Level.SILVER) {
+			user.setLevel(Level.GOLD);
+		}
+		this.userDao.update(user);
 	}
 }
