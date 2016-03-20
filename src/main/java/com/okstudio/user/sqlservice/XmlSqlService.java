b0c6718122946fbdd1dff837301d16a4764b8023
Kim Jong Rok <jrkim123us@gmail.com>
2016-03-20 14:51:30 +0900

    빈의 초기화 작업 (@PostConstruct)

--- src/main/java/com/okstudio/user/sqlservice/XmlSqlService.java
+++ src/main/java/com/okstudio/user/sqlservice/XmlSqlService.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -14,13 +15,18 @@ import com.okstudio.user.sqlservice.jaxb.Sqlmap;
 
 public class XmlSqlService implements SqlService {
 	private Map<String, String> sqlMap = new HashMap<String, String>();
+	private String sqlmapFile;
 	
 	public XmlSqlService() {
+	}
+	
+	@PostConstruct
+	public void loadSql() {
 		String contextPath = Sqlmap.class.getPackage().getName();
 		try {
 			JAXBContext context = JAXBContext.newInstance(contextPath);
 			Unmarshaller unmarshaller = context.createUnmarshaller();
-			InputStream inputStream = UserDao.class.getResourceAsStream("sqlmap.xml");
+			InputStream inputStream = UserDao.class.getResourceAsStream(this.sqlmapFile);
 			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(inputStream);
 			
 			for(SqlType sql : sqlmap.getSql()) {
@@ -28,7 +34,10 @@ public class XmlSqlService implements SqlService {
 			}
 		} catch(JAXBException e) {
 			throw new RuntimeException(e);
-		}
+		}		
+	}
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
 	}
 
 	@Override
@@ -40,5 +49,6 @@ public class XmlSqlService implements SqlService {
 			return sql;			
 		}
 	}
+	
 
 }
