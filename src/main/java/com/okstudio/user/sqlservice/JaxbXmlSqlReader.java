ad2b9a3748f5210732e9d9f766128d43a093bd20
Kim Jong Rok <jrkim123us@gmail.com>
2016-03-20 15:49:13 +0900

    디폴트 의존관계 (확장 가능한 기반 클래스)

---
+++ src/main/java/com/okstudio/user/sqlservice/JaxbXmlSqlReader.java
@@ -0,0 +1,35 @@
+package com.okstudio.user.sqlservice;
+
+import java.io.InputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import com.okstudio.user.dao.UserDao;
+import com.okstudio.user.sqlservice.jaxb.SqlType;
+import com.okstudio.user.sqlservice.jaxb.Sqlmap;
+
+public class JaxbXmlSqlReader implements SqlReader {
+	private String sqlmapFile;
+	
+	public void setSqlmapFile(String sqlmapFile) {
+		this.sqlmapFile = sqlmapFile;
+	}
+	@Override
+	public void read(SqlRegistry sqlRegistry) {
+		String contextPath = Sqlmap.class.getPackage().getName();
+		try {
+			JAXBContext context = JAXBContext.newInstance(contextPath);
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+			InputStream inputStream = UserDao.class.getResourceAsStream(this.sqlmapFile);
+			Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(inputStream);
+			
+			for(SqlType sql : sqlmap.getSql()) {
+				sqlRegistry.registerSql(sql.getKey(), sql.getValue());
+			}
+		} catch(JAXBException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
