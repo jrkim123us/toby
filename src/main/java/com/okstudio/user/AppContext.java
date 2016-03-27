b42e5db1125f89a4c0a4075ddcc6dabe52a0e2ec
Kim Jong Rok <jrkim123us@gmail.com>
2016-03-27 17:27:47 +0900

    프로퍼티 소스 (타입 01)

--- src/main/java/com/okstudio/user/AppContext.java
+++ src/main/java/com/okstudio/user/AppContext.java
@@ -3,11 +3,14 @@ package com.okstudio.user;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -15,7 +18,6 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.okstudio.user.dao.UserDao;
 import com.okstudio.user.service.DummyMailSender;
 import com.okstudio.user.service.UserService;
 import com.okstudio.user.service.UserServiceTest.TestUserServiceImpl;
@@ -24,17 +26,40 @@ import com.okstudio.user.service.UserServiceTest.TestUserServiceImpl;
 @EnableTransactionManagement
 @ComponentScan(basePackages="com.okstudio.user")
 @Import(SqlServiceContext.class)
+@PropertySource("/database.properties")
 public class AppContext {
-	@Autowired UserDao userDao;
+//	@Autowired UserDao userDao;
+	
+	@Autowired Environment env;
+	
+//	@Value("${db.driverClass}") Class<? extends Driver> driverClass;
+//	@Value("${db.url}") String url;
+//	@Value("${db.username}") String username;
+//	@Value("${db.password}") String password;
 	
 	@Bean
 	public DataSource dataSource() {
 		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 		
-		dataSource.setDriverClass(org.mariadb.jdbc.Driver.class);
-		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/toby?characterEncoding=UTF-8");
-		dataSource.setUsername("root");
-		dataSource.setPassword("");
+//		dataSource.setDriverClass(org.mariadb.jdbc.Driver.class);
+//		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/toby?characterEncoding=UTF-8");
+//		dataSource.setUsername("root");
+//		dataSource.setPassword("");
+		
+		try {
+			dataSource.setDriverClass((Class<? extends java.sql.Driver>)Class.forName(env.getProperty("db.driverClass")));
+		}
+		catch(ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+		dataSource.setUrl(env.getProperty("db.url"));
+		dataSource.setUsername(env.getProperty("db.username"));
+		dataSource.setPassword(env.getProperty("db.password"));
+		
+//		dataSource.setDriverClass(this.driverClass);
+//		dataSource.setUrl(this.url);
+//		dataSource.setUsername(this.username);
+//		dataSource.setPassword(this.password);
 		
 		return dataSource;
 	}
@@ -44,6 +69,11 @@ public class AppContext {
 		DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
 		transactionManager.setDataSource(dataSource());
 		return transactionManager;
+	}
+	
+	@Bean
+	public static PropertyPlaceholderConfigurer placeholderConfigure() {
+		return new PropertyPlaceholderConfigurer();
 	}
 	
 	@Configuration
